07af5115361e19d6ac7d4594b2b45c8166f84a2f
Konurbaeva <konurbaevamadina@gmail.com>
2022-02-05 11:55:27 +0100

    solve 2-3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
 
+import throttle from 'lodash.throttle';
+
+
+const LOCAL_STORAGE_KEY = 'feedback-msg';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+
+if(localStorage.getItem(LOCAL_STORAGE_KEY) !== null) {
+  console.log('local storage');
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const {
+    elements: { email, message }
+  } = evt.currentTarget;
+
+console.log("email", email);
+console.log("message", message);
+console.log("evt.currentTarget", evt.currentTarget);
+console.log("evt.currentTarget.value", evt.currentTarget.value);
+
+  // console.log("send form");
+  // evt.currentTarget.reset();
+  // localStorage.removeItem(LOCAL_STORAGE_KEY);
+
+ // localStorage.setItem(LOCAL_STORAGE_KEY, values);
+}
+
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, message);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
+
+populateTextarea();
